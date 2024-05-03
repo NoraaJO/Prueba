c0a426ae4b2dfccc2e322f77fc7aded6f4ac3743
NoraaJO <aaronortizjimenez@gmail.com>
2024-05-03 13:43:33 -0600

    Se corrige el error de requiere

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const cors = requiere("cors");
+const cors = require("cors");
 const { studentsRouter } = require("./modules/students");
 const { professorsRouter } = require("./modules/professors");
 const { activitiesRouter } = require("./modules/activities");
